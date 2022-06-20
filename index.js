905c1999dbde117d6f216b25084127ac8946ae99
ArnazXyz <104475727+ArnazXyz@users.noreply.github.com>
2022-06-20 19:05:04 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const fetch = require('node-fetch');
+const moment = require('moment');
+const chalk = require('chalk');
+const rs = require('readline-sync');
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const GoStumble = (auth) => new Promise((resolve, reject) => {
+
+  fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
+    method: 'GET',
+    headers: {
+      'authorization': auth
+    }
+  })
+    .then(res => res.text())
+    .then(data => {
+      resolve(data);
+    })
+    .catch(err => {
+      reject(err);
+    });
+
+});
+
+(async () => {
+
+  console.log(`Trophy And Crown Hack Safe!
+By : ${chalk.bold('Arnazx#9611')} - Credit : @dkmpostor & @Eskey & @Arnazx 
+`);
+
+  const auth = rs.question('Enter Authentication Code! : ');
+  console.log('');
+
+  while (true) {
+
+    const result = await GoStumble(auth);
+    if (!result) {
+
+      console.log(chalk.red(`\r[ ${moment().format('HH:mm:ss')} ] Authentication Code Not Valid`));
+      break;
+
+    } else if (result.includes('User')) {
+
+      const data = JSON.parse(result);
+      const username = data.User.Username;
+      const country = data.User.Country;
+      const trophy = data.User.SkillRating;
+      const crown = data.User.Crowns;
+
+console.log(chalk.bgBlack(`\r[ ${moment().format('HH:mm:ss')} ] ${chalk.white(`User : ${username}`)} | ${chalk.yellow(`Trophy : ${trophy}`)} | ${chalk.red(`Crown : ${crown}`)}`));
+      await sleep(7000);
+
+    } else if (result == 'BANNED') {
+      console.log(chalk.bgRed(`Mampus ke banned akunnya aowokaw:v`));
+     break;
+    }
+  }
+
+
+})();
